fce8a4157a081edd39036714c1896fbe83dbd4a4
saianuragt <saianuragt@gmail.com>
2020-07-02 01:44:34 +0530

    FSD0620B105

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.PersonalityCalculator;
+
 
 @WebServlet(urlPatterns= {"/personality"})
 public class PersonalityViewController extends HttpServlet {
@@ -33,6 +35,11 @@ public class PersonalityViewController extends HttpServlet {
 		String options=request.getParameter("selectedOptions");
 		
 		System.out.println(options);
+		
+		PersonalityCalculator obj = new PersonalityCalculator();
+		String message = obj.findYourBrainType(options);
+		request.setAttribute("message",message);
+		System.out.println("You are "+message);
 				
 		/* 
 
